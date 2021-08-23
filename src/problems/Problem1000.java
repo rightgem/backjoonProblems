3bdfa95e85982abc4c6e78c4a91c4fbd04bd8710
rightgem <62364511+rightgem@users.noreply.github.com>
2021-08-23 23:31:16 +0900

    1000번
    A+B

---
+++ src/problems/Problem1000.java
@@ -0,0 +1,18 @@
+package problems;
+
+import java.util.Scanner;
+
+public class Problem1000 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a + b);		
+		
+	}
+
+}
